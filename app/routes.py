bc353e1ed7c2c2e8d5dbd101a9a0631c3a6bc4bb
Kim Placha <kimberly.placha@sdgku.edu>
2024-04-17 19:46:29 -0700

    1st

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get("/about")
 @app.get("/")
-def index ():
-    "first_name": "Kim"
-    "last_name": "Placha"
+def index():  # sourcery skip: inline-immediately-returned-variable
+    me = {
+        "first_name": "Kim",  # type: ignore
+        "last_name": "Placha",
+        "hobbies": "Arts and Crafts",
+        "is_online": True,
+    }
+    return me
+    
